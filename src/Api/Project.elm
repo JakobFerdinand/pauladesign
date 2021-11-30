45ddc64526f6ea28fd515e0497ceddc6f9d50b5b
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2021-11-30 09:15:17 +0100

    Project detail view

--- src/Api/Project.elm
+++ src/Api/Project.elm
@@ -1,4 +1,4 @@
-module Api.Project exposing (Project, allProjects)
+module Api.Project exposing (Project, ProjectDetails, allProjects, projectDetails)
 
 import DataSource exposing (DataSource)
 import DataSource.Http
@@ -8,9 +8,9 @@ import Pages.Secrets as Secrets
 
 type alias Project =
     { title : String
-    , imageUrl : String
+    , mainImageUrl : String
     , slug : String
-    , description : Maybe String
+    , projectDescription : Maybe String
     }
 
 
@@ -25,6 +25,44 @@ allProjects =
                     (Decode.field "imageUrl" Decode.string)
                     (Decode.field "slug" Decode.string)
                     (Decode.maybe (Decode.field "description" Decode.string))
+                )
+            )
+        )
+
+
+type alias ProjectDetails =
+    { title : String
+    , mainImageUrl : String
+    , imageUrls : List String
+    , projectDescription : Maybe String
+    , enhancedProjectDescription : Maybe String
+    }
+
+
+
+-- GROQ Query
+-- *[_type == 'project' && slug.current == 'jakobs-projekt'] {
+--  title,
+--  "mainImageUrl": mainImage.asset->url,
+--  projectDescription,
+--  "imageUrls": images[].asset->url,
+--  enhancedProjectDescription
+--}
+
+
+projectDetails : String -> DataSource ProjectDetails
+projectDetails slug =
+    DataSource.Http.get
+        (Secrets.succeed ("https://htusge0f.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20'project'%20%26%26%20slug.current%20%3D%3D%20'" ++ slug ++ "'%5D%20%7B%0A%20%20title%2C%0A%20%20%22mainImageUrl%22%3A%20mainImage.asset-%3Eurl%2C%0A%20%20projectDescription%2C%0A%20%20%22imageUrls%22%3A%20images%5B%5D.asset-%3Eurl%2C%0A%20%20enhancedProjectDescription%0A%7D"))
+        (Decode.field "result"
+            (Decode.index
+                0
+                (Decode.map5 ProjectDetails
+                    (Decode.field "title" Decode.string)
+                    (Decode.field "mainImageUrl" Decode.string)
+                    (Decode.field "imageUrls" (Decode.list Decode.string))
+                    (Decode.maybe (Decode.field "projectDescription" Decode.string))
+                    (Decode.maybe (Decode.field "enhancedProjectDescription" Decode.string))
                 )
             )
         )
--- src/Page/Projects/Project_.elm
+++ src/Page/Projects/Project_.elm
@@ -1,13 +1,13 @@
-module Page.Projects exposing (Data, Model, Msg, page)
+module Page.Projects.Project_ exposing (Data, Model, Msg, page)
 
-import Api.Project exposing (Project)
+import Api.Project
 import DataSource exposing (DataSource)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Font as Font
 import Head
 import Head.Seo as Seo
-import Markdown exposing (TableOfContent, markdown)
+import Markdown exposing (markdown)
 import Page exposing (Page, PageWithState, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
@@ -24,25 +24,28 @@ type alias Msg =
 
 
 type alias RouteParams =
-    {}
+    { project : String }
 
 
 page : Page RouteParams Data
 page =
-    Page.single
+    Page.prerender
         { head = head
+        , routes = routes
         , data = data
         }
         |> Page.buildNoState { view = view }
 
 
-type alias Data =
-    List Project
-
-
-data : DataSource Data
-data =
+routes : DataSource (List RouteParams)
+routes =
     Api.Project.allProjects
+        |> DataSource.map (List.map (\project -> { project = project.slug }))
+
+
+data : RouteParams -> DataSource Data
+data routeParams =
+    Api.Project.projectDetails routeParams.project
 
 
 head :
@@ -65,49 +68,54 @@ head static =
         |> Seo.website
 
 
+type alias Data =
+    Api.Project.ProjectDetails
+
+
 view :
     Maybe PageUrl
     -> Shared.Model
     -> StaticPayload Data RouteParams
     -> View Msg
 view maybeUrl sharedModel static =
-    { title = "Projects"
+    { title = static.data.title
     , body =
-        [ column
-            [ height fill
-            , centerX
-            , padding 30
-            , spacing 20
-            ]
-            [ el [ centerX, alignTop ] <| text "Projects"
-            , el [ alignTop ]
-                (text ("Anzahl: " ++ (static.data |> List.length |> String.fromInt)))
-            , column
+        [ el [ scrollbarY, height fill, width fill ] <|
+            column
                 [ centerX
                 , height fill
-                , scrollbarY
+                , spacing 20
                 ]
-                (static.data |> List.map viewProject)
-            ]
-        ]
-    }
-
-
-viewProject : Project -> Element msg
-viewProject project =
-    link []
-        { url = "/projects/" ++ project.slug
-        , label =
-            column
-                []
-                [ el [ Font.bold ] <| text project.title
+                [ el [ Font.bold, centerX ] <| text static.data.title
                 , el
-                    [ width <| px 400
-                    , height <| px 400
-                    , Background.image project.mainImageUrl
+                    [ height <| px 400
+                    , width <| px 400
+                    , Background.image static.data.mainImageUrl
                     ]
                     none
-                , case project.projectDescription of
+                , case static.data.projectDescription of
+                    Just description ->
+                        case markdown <| String.replace "\u{000D}" "" description of
+                            Ok ( toc, renderedEls ) ->
+                                column
+                                    [ spacing 30
+                                    , padding 10
+                                    , centerX
+                                    , width fill
+                                    ]
+                                    renderedEls
+
+                            Err errors ->
+                                paragraph []
+                                    [ text "I´m sorry but it looks like I published invalid markdown."
+                                    , text "Feel free to contact me so I can updated and fix the problem."
+                                    ]
+
+                    Nothing ->
+                        none
+                , row [ spacing 10 ]
+                    (static.data.imageUrls |> List.map (\url -> el [ width <| px 250, height <| px 250, Background.image url ] none))
+                , case static.data.enhancedProjectDescription of
                     Just description ->
                         case markdown <| String.replace "\u{000D}" "" description of
                             Ok ( toc, renderedEls ) ->
@@ -128,4 +136,5 @@ viewProject project =
                     Nothing ->
                         none
                 ]
-        }
+        ]
+    }
